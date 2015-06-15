511dffeab6217ca96b25dd7ca2eb1aa3e5a11e8a
Tom W Hartung <tomwhartung@gmail.com>
2015-06-14 20:07:23 -0600

    Changed to call new displayDeviceData method in jmwsIdMyGadget .

--- templates/jmws_beez3_idmygadget/index.php
+++ templates/jmws_beez3_idmygadget/index.php
@@ -118,8 +118,7 @@ $doc->addScript($this->baseurl . '/templates/' . $this->template . '/javascript/
 			<p>$jmwsIdMyGadget->getGadgetDetector() = <?php echo $jmwsIdMyGadget->getGadgetDetector() ?></p>
 			<?php if ( $jmwsIdMyGadget->isInstalled() ) : ?>
 				<p>$jmwsIdMyGadget->getGadgetString() = <?php echo $jmwsIdMyGadget->getGadgetString() ?></p>
-				<p>$jmwsIdMyGadget->getIdMyGadget()->displayDeviceData() =
-					<?php echo $jmwsIdMyGadget->getIdMyGadget()->displayDeviceData() ?></p>
+				<p>$jmwsIdMyGadget->displayDeviceData() = <?php echo $jmwsIdMyGadget->displayDeviceData() ?></p>
 			<?php else : ?>
 				<p>The <?php echo $jmwsIdMyGadget->getGadgetDetector() ?> detector is not installed.
 					For information about how to install idMyGadget detectors,
